40403f6e04c120e1e6765e8b0882e10dbb8ef868
LovelyCat <1252251361@qq.com>
2025-03-24 22:34:59 +0800

    fix: node tasks probe returns error

--- CrystalServer/crystal-common/src/main/kotlin/com/lovelycatv/ai/crystal/common/vo/NodeTaskVO.kt
+++ CrystalServer/crystal-common/src/main/kotlin/com/lovelycatv/ai/crystal/common/vo/NodeTaskVO.kt
@@ -1,12 +1,17 @@
 package com.lovelycatv.ai.crystal.common.vo
 
+import com.lovelycatv.ai.crystal.common.data.message.MessageChain
+
 /**
  * @author lovelycat
  * @since 2025-02-28 19:56
  * @version 1.0
  */
 data class NodeTaskVO(
+    val sessionId: String,
     val taskType: String,
     val taskClass: String,
-    val data: Any
+    val expireTime: Long,
+    val priority: Int,
+    val originalMessages: MessageChain
 )
