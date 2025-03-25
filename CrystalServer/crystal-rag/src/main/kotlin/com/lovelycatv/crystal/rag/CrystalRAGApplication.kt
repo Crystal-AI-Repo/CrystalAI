ffbbe7d21c38e60500d91c61d342c0df2e708656
LovelyCat <1252251361@qq.com>
2025-03-25 15:06:06 +0800

    initial commit

---
+++ CrystalServer/crystal-rag/src/main/kotlin/com/lovelycatv/crystal/rag/CrystalRAGApplication.kt
@@ -0,0 +1,11 @@
+package com.lovelycatv.crystal.rag
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CrystalRAGApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(CrystalRAGApplication::class.java, *args)
+}
