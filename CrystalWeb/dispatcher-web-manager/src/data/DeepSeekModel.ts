a5f68f00dbd447f1c526f1fe91960023722b1835
LovelyCat <1252251361@qq.com>
2025-04-04 19:51:04 +0800

    dispatcher-web-manager

---
+++ CrystalWeb/dispatcher-web-manager/src/data/DeepSeekModel.ts
@@ -0,0 +1,3 @@
+export interface DeepSeekModel {
+    id: string
+}
---
+++ CrystalWeb/dispatcher-web-manager/src/locales/en.ts
@@ -0,0 +1,44 @@
+export default {
+    application: {
+        name: 'Crystal Dispatcher Manager'
+    },
+    asideNavigation: {
+        home: 'Home',
+        nodes: 'Nodes'
+    },
+    page: {
+        nodeManager: {
+            title: 'Node Manager',
+            table: {
+                headers: {
+                    nodeId: 'Node ID',
+                    nodeName: 'Node Name',
+                    nodeAddress: 'Node Address',
+                    nodeRegisteredTime: 'Registered Time',
+                    lastAliveTimestamp: 'Last Alive',
+                    nodeAliveness: 'Aliveness',
+                    nodeNettyConnection: 'Netty',
+                    modelName: 'Display Name',
+                    qualifiedModelName: 'Model Name',
+                    modelQuantization: "Quantization",
+                    modelSize: 'Size',
+                    modelDigest: 'Digest',
+                    modelModifiedTime: 'Modified Time'
+                },
+                values: {
+                    secondsAgo: '{p0}s ago',
+                    ollamaModels: 'Ollama Models',
+                    deepseekModels: 'DeepSeek Models',
+                    disconnected: 'Disconnected',
+                    online: 'Online',
+                    offline: 'Offline'
+                }
+            }
+        },
+        nodeDetails: {
+            message: {
+                couldNotFetchNodeDetails: 'Could not fetch node details'
+            }
+        }
+    }
+}
